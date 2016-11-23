7667d0504b99b048fd8018b334392856d943e27d
MarkIsNotTaken <mdemesillo@gmail.com>
2016-11-23 08:53:13 -1000

    js_test_review.js

--- js_test_review.js
+++ js_test_review.js
@@ -2,13 +2,14 @@
 //Mark Demesillo 
 //11.23
 
-var knight = "Gawain"
-var maiden = "Morgana"
-var monster = "Loch Ness"
+var knight = "Gawain";
+var maiden = "Morgana";
+var monster = "Loch Ness";
 
-var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"]
-
-var hero = []
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
+var wizard = "Merlin";
+var weapon = weaponChest[0];
+var power = 50;
 
 function swordInTheStone(worthy){
 	if(worthy == "Arthur"){
@@ -19,3 +20,25 @@ function swordInTheStone(worthy){
 }
 
 swordInTheStone("Gawain");
+
+function trueLove(king, queen){	
+	if(queen == "Quinevere" && king == "Arthur"){
+		console.log("On this day, our " + king + " and  " + queen + " shall rule happily ever after.")
+	}else{
+		console.log("We must keep searching, for true love must reign over Camelot.")
+	}
+}
+
+trueLove(knight, maiden);
+
+function epicBattle(wizard, enemy, power){
+	if(power <= 15){
+		console.log("Our " + wizard + " has fallen.")
+	}else if(power >= 16 && power >= 40){
+		console.log(wizard + " has almost slain " + enemy + ".")
+	}else{
+		console.log("Our " + wizard + " has slain the " + enemy + ".")
+	}
+}
+
+epicBattle(wizard, monster, power);
